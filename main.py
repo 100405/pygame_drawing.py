7aef572a11f0d118d3cc9e35277c35309c4cb02c
tony lee <kaspurly@gmail.com>
2021-11-18 11:17:44 -0800

    Fixed issues regarding the dvd image and the background image

--- main.py
+++ main.py
@@ -12,10 +12,51 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 540
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-WINDOW_TITLE = "Pygame Drawing"
+WINDOW_TITLE = "DVD Screen Saver"
+
+class Dvdimage:
+    """Represents a dvdimage on screen
+
+    Attributes:
+        x, y coordinates of top-left corner
+        width: with of our rectangle in px
+        height: height of our rectangle in px
+        colour: hex value of rgb"""
+    def __init__(self):
+        self.x, self.y = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        self.width = 180
+        self.height = 180
+        self.img = pygame.image.load("./images1/download.png")
+        self.x_vel = 5
+        self.y_vel = 5
+
+    def rect(self) -> pygame.rect:
+        """Returns a pygame.rect that represents the dvd_image"""
+        return [self.x, self.y, self.width, self.height]
+
+    def update(self) -> None:
+        self.x += self.x_vel
+        if self.x < 0:
+            self.x = 0
+            self.x_vel = -self.x_vel
+        if self.x + self.width > SCREEN_WIDTH:
+            self.x = SCREEN_WIDTH - self.width
+            self.x_vel = -self.x_vel
+
+        self.y += self.y_vel
+        if self.y < 0:
+            self.y = 0
+            self.y_vel = -self.y_vel
+        if self.y + self.width > SCREEN_HEIGHT:
+            self.y = SCREEN_HEIGHT - self.width
+            self.y_vel = -self.y_vel
+
+
+
 def main() -> None:
     """Driver of the Python script"""
     # Create the screen
@@ -25,6 +66,8 @@ def main() -> None:
     # Create some local variables that describe the environment
     done = False
     clock = pygame.time.Clock()
+    dvd_image = Dvdimage()
+    bg_image = pygame.image.load('images1/uzwq176ixfpslxkgxl1i.webp')
 
     # Create the main loop
     while not done:
@@ -33,11 +76,21 @@ def main() -> None:
             if event.type == pygame.QUIT:
                 done = True
         # Logic (Change the environment)
+        dvd_image.update()
+
+        # print(f"x: {download.png.x}36+ ")
 
         # Draw the environment
-            screen.fill(WHITE)  # fill with bgcolor
-        # Update the screen
-            pygame.display.flip()
+        screen.fill(WHITE)  # fill with bgcolor
+
+        # background
+        bg_image = pygame.transform.scale(bg_image, (960, 540))
+        screen.blit(bg_image, (0,0))
+
+        # dvdimage
+        screen.blit(dvd_image.img , (dvd_image.x, dvd_image.y))
+
+        pygame.display.flip()
         # Tick the clock
         clock.tick(60)
 if __name__ == "__main__":
